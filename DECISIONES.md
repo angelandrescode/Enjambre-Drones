41ec9917757c69dd6879f317c0fb13deca470e20
Angel Andres <andressamaniegosandoval@gmail.com>
2026-09-04 20:37:06 +0200

    pef(fase0):se implementa la influencia del viento con 8 convoluciones

--- DECISIONES.md
+++ DECISIONES.md
@@ -1,4 +1,6 @@
 # FASE 0
 
-1. Se usaron convoluciones en ves de bucles dentro de bucles para mejorar rendimiento.
+1. Se usaron convoluciones en ves de bucles dentro de bucles para mejorar rendimiento. Al hacer esto pasamos de 400seg a 2seg en que se termine de ejecutar todo el codigo (osea cuando toda la grilla esta incendiada)
 2. Se uso la formula 1-(1-p)^n. Esta formula da la probabilidad de que suceda un evento con probabilidad p que suceda en n intentos. Se puede interpretar como: P es la probabilidad de que un evento suceda y N el factor que usamos para que ese evento tenga mas probabilidad de suceder. En nuestro caso, p es la probabilidad base de que una celda se incendie, y n es la cantidad de vecinos, los cuales mientras mas haya mas aumenta la probabilidad de que una celda se incendie.
+3. Se usan 8 convoluciones por direccion para tener la capacidad de comparar cada dirección con el viento y sacar una probabilidad total entre si con la formula de antes (1-(1-p)^n) pero ahora n es un numero fijo, que es 8.
+4. Se usa la convencion fila, columna. (osea, y,x) para mantener la consistencia con numpy.
--- main.py
+++ main.py
@@ -3,34 +3,47 @@ import time
 from scipy.signal import convolve2d
 
 inicio = time.time()
-rows, columns = 10, 10
+rows, columns = 200, 200
 grid = np.zeros((rows, columns))
 
 grid[0, 0] = 1  # fuego semilla inicial
 
 base_probability = 0.1
 wind_force = 1 # si la extension del fuego esta alineada con la direccion del viento, entonces, habra un 100% de que se encienda ese fuego
-wind_vector = np.array([5,0]) 
+wind_vector = np.array([-5,0]) 
 normalized_wind_vector = wind_vector / np.linalg.norm(wind_vector) #solo hallamos la direccion del viento, que es lo que nos importa
 
-
-#TODO: Falta aplicar la fuerza del viento, actualmente solo hay una "probabilidad base"
 def start_simulation(initial_grid):
-    new_grid = initial_grid
-    #Indica que celdas tomar en cuenta por la convolución
-    kernel = np.array([[1, 1, 1],
-                       [1, 0, 1],
-                       [1, 1, 1]])
-    
+    pattern_row = [0, 0, 1, 1, 1, -1, -1, -1]
+    pattern_column = [1, -1, 0, 1, -1, 0, 1, -1]
+    new_grid = initial_grid.copy()
+    template_kernel = np.zeros((3, 3))
+    array_of_convolutions = []
     while not new_grid.all():
-        neighbors = convolve2d(new_grid, kernel, mode='same', boundary='fill')
+        for i in range(8):
+            template_kernel[1+pattern_row[i], 1+pattern_column[i]] = 1
+            kernel_in_actual_direction = template_kernel.copy()
+            
+            neighbors = convolve2d(new_grid, kernel_in_actual_direction, mode='same', boundary='fill')
+            vector_fire_expansion = np.array([pattern_row[i], pattern_column[i]])
+            normalized_vector_fire_expansion = vector_fire_expansion / np.linalg.norm(vector_fire_expansion)
+            similarity_to_wind_vector = np.dot(normalized_vector_fire_expansion, normalized_wind_vector)
+            
+            probability_to_get_burnt = np.clip(base_probability + (1 - base_probability) * wind_force * similarity_to_wind_vector, base_probability, 1.0)
+            neighbors = neighbors * probability_to_get_burnt
+            array_of_convolutions.append(neighbors)
+            template_kernel[1+pattern_row[i], 1+pattern_column[i]] = 0
+        
+        prob_total = 1 - np.prod(1 - np.array(array_of_convolutions), axis=0)
         random_array = np.random.random(new_grid.shape)
-        # Si esta incendiadada: que se mantenga incendiada
-        # Si esta apagada: La probabilidad de que se encienda aumenta segun la cantidad de vecinos tenga a su alrededor.        
-        new_grid_in_boolean = (new_grid == 1) | ((new_grid == 0) & ((1 - (1 - base_probability) ** neighbors) > random_array))
-        new_grid = new_grid_in_boolean.astype(int)
-    return new_grid
-
+        success_mask = random_array < prob_total
+        new_grid[success_mask] = 1
+        array_of_convolutions = []
+            
+            
+            
+            
+            
+            
 start_simulation(grid)
-
-
+print(time.time() - inicio)
